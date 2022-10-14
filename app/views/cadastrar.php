4d100d637a4fe92e842673fa21b645d5b416672d
Fabiano <fabiano.ssantos79@gmail.com>
2022-10-14 11:12:48 -0300

    Update cadastrar.php

--- app/views/cadastrar.php
+++ app/views/cadastrar.php
@@ -9,7 +9,7 @@
 <form action="" method="POST">
     Título: <input type="text" name="titulo" /><br><br>
     Mensagem: <textarea name="texto" id=""></textarea><br><br>
-    <button name="btn-cadastrar">Cadastrar Novo</button>
+    <button name="btn-cadastrar">Cadastro</button>
 </form>
 <br>
 <button><a href="http://localhost:8080/">Voltar</a></button>
